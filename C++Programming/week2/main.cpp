a90e4b48edd80b62218a4e41b32ecf5a41461f7d
orbit3230 <hu0868@naver.com>
2024-03-13 21:40:20 +0900

    week2_v1

--- C++Programming/week2/main.cpp
+++ C++Programming/week2/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool game(int);
+bool game(string);
 
 // 게임을 실제로 실행시키는 main() 함수가 들어있는 메인 파일이다.
 int main() {
-    int answer;
+    string answer;
     cout << "Enter a answer: " << ends;
     cin >> answer;
     while(true) {
         if(game(answer))
             break;
     }
-    cout << "You win! " << endl;
+    cout << "You win!" << endl;
 }
