a386cfd4d7439e6f6ac156bc41969b26a0a71570
orbit3230 <hu0868@naver.com>
2024-03-13 15:00:01 +0900

    week2_v1

---
+++ C++Programming/week2/game.cpp
@@ -0,0 +1,34 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+int judge(int, int);
+
+// 게임 흐름, 스트라이크/볼 처리 등을 다루는 함수들이 들어있다.
+bool game(int answer) {
+    int input;
+    cout << "Enter a guess: ";
+    cin >> input;
+    if(judge(input, answer) == 3)
+        return true;
+    else
+        return false;
+}
+int judge(int input, int answer) {
+    int strikes = 0;
+    int balls = 0;
+    string i = to_string(input);
+    string a = to_string(answer);
+
+    for(int s = 0 ; s < 3 ; s++)
+        for(int b = 0 ; b < 3 ; b++) {
+            if(s == b && i[s] == a[b]) {
+                strikes++;
+                break;
+            }
+            else if(i[s] == a[b])
+                balls++;
+    }
+    cout << "Strikes: " << strikes << ", Balls: " << balls << endl;
+    return strikes; 
+}
