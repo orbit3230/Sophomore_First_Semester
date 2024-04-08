afb60448e58f7ffe8f9f9e796417f978fc83d6b1
orbit3230 <hu0868@naver.com>
2024-04-08 12:14:33 +0900

    Update doubly_linkedlist.c

--- Data_Structure/week4/doubly_linkedlist.c
+++ Data_Structure/week4/doubly_linkedlist.c
@@ -21,6 +21,22 @@ void dinsert(DListNode* before, element value) {
     before->rlink->llink = newnode;
     before->rlink = newnode;
 }
+void dinsert_right(DListNode* before, element value) {
+    DListNode* newnode = (DListNode*)malloc(sizeof(DListNode));
+    newnode->data = value;
+    newnode->llink = before;
+    newnode->rlink = before->rlink;
+    before->rlink->llink = newnode;
+    before->rlink = newnode;
+}
+void dinsert_left(DListNode* after, element value) {
+    DListNode* newnode = (DListNode*)malloc(sizeof(DListNode));
+    newnode->data = value;
+    newnode->rlink = after;
+    newnode->llink = after->llink;
+    after->llink->rlink = newnode;
+    after->llink = newnode;
+}
 // 노드 삭제
 void dremove(DListNode* head, DListNode* removed) {
     if(removed == head) return;
@@ -30,6 +46,10 @@ void dremove(DListNode* head, DListNode* removed) {
 }
 // 이중 링크드 리스트 출력
 void print_dlist(DListNode* head) {
+    if(head->llink == head && head->rlink == head) {
+        printf("empty\n");
+        return;
+    }
     DListNode* p;
     for(p = head->rlink; p != head; p = p->rlink) {
         printf("<-| |%d| |-> ", p->data);
@@ -41,13 +61,13 @@ int main() {
     DListNode* head = (DListNode*)malloc(sizeof(DListNode));
     init(head);
     printf("Add nodes\n");
-    for(int i = 0; i < 5; i++) {
-        // 헤드 노드의 오른쪽에 삽입
-        dinsert(head, i);
+    for(int i = 0 ; i < 6 ; i++) {
+        if(i % 2 == 0) dinsert_left(head, i);
+        else dinsert_right(head, i);
         print_dlist(head);
     }
     printf("\nRemove nodes\n");
-    for(int i = 0; i < 5; i++) {
+    for(int i = 0; i < 7; i++) {
         print_dlist(head);
         dremove(head, head->rlink);
     }
