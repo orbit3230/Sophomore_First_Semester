eed89c5c216b63f3f32dc32967c19b5059b94ce1
orbit3230 <hu0868@naver.com>
2024-05-30 23:44:41 +0900

    week13

--- Data_Structure/week13/town.c
+++ Data_Structure/week13/town.c
@@ -6,6 +6,8 @@ typedef struct GraphType {
     int *visited;
 } GraphType;
 
+int max_dist = 0;
+
 void init(GraphType* g, int n) {
     g->n = 0;
     g->adj_mat = (int**)malloc(sizeof(int*) * n);
@@ -23,7 +25,7 @@ void init(GraphType* g, int n) {
 
 void insert_vertex(GraphType* g, int v) {
     if (((g->n) + 1) > 100) {
-        printf("그래프 정점의 개수 초과\n");
+        printf("Vertex Overflow\n");
         return;
     }
     g->n++;
@@ -31,21 +33,38 @@ void insert_vertex(GraphType* g, int v) {
 
 void insert_edge(GraphType* g, int start, int end, int weight) {
     if (start >= g->n || end >= g->n) {
-        printf("정점 번호 오류\n");
+        printf("Vertex does not exist\n");
         return;
     }
     g->adj_mat[start][end] = weight;
     g->adj_mat[end][start] = weight;
 }
 
-void dfs_search(GraphType* g, int v, int visited[]) {
+// 가장 거리가 먼 두 마을을 찾을 것이다.
+int dfs_search(GraphType* g, int v, int visited[]) {
     visited[v] = 1;
-    printf("%d ", v);
-    for (int w = 0; w < g->n; w++) {
-        if (g->adj_mat[v][w] && !visited[w]) {
-            dfs_search(g, w, visited);
+    int max = 0;
+    int second_max = 0;
+    for (int i = 0; i < g->n; i++) {
+        if (g->adj_mat[v][i] != 0 && visited[i] == 0) {
+            int path_length = g->adj_mat[v][i] + dfs_search(g, i, visited);
+            if (path_length > max) {
+                second_max = max;
+                max = path_length;
+            }
+            else if (path_length > second_max) {
+                second_max = path_length;
+            }
         }
     }
+
+    // 가장 긴 두 경로의 합이 최대 거리가 된다.
+    if (max + second_max > max_dist) {
+        max_dist = max + second_max;
+    }
+
+    // 가장 긴 경로의 길이를 반환한다.
+    return max;
 }
 
 int main() {
@@ -66,5 +85,7 @@ int main() {
             insert_vertex(&g, v);
             insert_edge(&g, u, v, d);
         }
+
+        printf("%d\n", dfs_search(&g, 0, g.visited));
     }
 }
